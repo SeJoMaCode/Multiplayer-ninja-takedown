ed519ad064b63a5da9f52454243fd7e3a604663d
SeJoMaCode <sethmanche02@gmail.com>
2021-03-24 13:05:40 -0500

    multiplayer

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 window.addEventListener('DOMContentLoaded', DOMContentLoaded => {
 
+    const socket = new WebSocket('wss://southwestern.media/game_dev'); 
+    socket.addEventListener('open', open => {
+        console.log('WEBSOCKET STARTED'); 
+    }); 
+
     // INITIALIZE CANVAS
     const render = document.querySelector('canvas').getContext('2d'); 
     const U_SCALE = 128; 
@@ -16,8 +21,6 @@ window.addEventListener('DOMContentLoaded', DOMContentLoaded => {
     // INITIALIZE IMAGE
     const player_avatar = new Image(); 
     player_avatar.src = 'images/player_spritesheet.png';
-    const enemy_avatar = new Image(); 
-    enemy_avatar.src = 'images/enemy_spritesheet.png';
 
     // PLAYER INPUT
     const movement = {ArrowRight: false, ArrowLeft: false, ArrowDown: false, ArrowUp: false}; 
@@ -31,7 +34,44 @@ window.addEventListener('DOMContentLoaded', DOMContentLoaded => {
             movement[keyup.key] = false; 
         }
     });
-    const emovement = {right: false, left: false, down: false, up: false};
+
+    const GAME = 'ninja_top_down'; 
+    const NAME = Math.random().toString(); 
+    const enemies = {}; 
+    const send = message => {
+        socket.send(JSON.stringify({Game: GAME, Name: NAME, Message: message})); 
+    }; 
+    socket.addEventListener('message', message => {
+        const parsed = JSON.parse(message.data); 
+        if(parsed.Game !== GAME || parsed.Name === NAME) {
+            return; 
+        }
+        console.log(parsed.Message)
+        if(parsed.Message === 'goodbye') {
+            console.log('GOODBYE'); 
+            delete enemies[parsed.Name]; 
+            return; 
+        }
+        enemies[parsed.Name] = JSON.parse(parsed.Message); 
+    }); 
+    const enemy_avatar = new Image(); 
+    enemy_avatar.src = 'images/enemy_spritesheet.png';
+    socket.addEventListener('beforeunload', beforeunload => {
+        send('goodbye'); 
+        beforeunload['returnValue'] = null; 
+    }); 
+
+    const patterns = {};
+    const floor = new Image()
+    floor.src = 'images/floor.png'
+    floor.addEventListener('load', load => {
+        patterns.floor = render.createPattern(floor, 'repeat')
+    })
+    const building = new Image()
+    building.src = 'images/building.png'
+    building.addEventListener('load', load => {
+        patterns.building = render.createPattern(building, 'repeat')
+    })
 
     // RIGID_BODY
     class Rigid_Body {
@@ -59,26 +99,22 @@ window.addEventListener('DOMContentLoaded', DOMContentLoaded => {
 
     // ANIMATION LOOP
     let frame_number = false; 
-    let frame_count = 0; 
-    let eframe_number = false; 
-    let eframe_count = 0; 
+    let frame_count = 0;
     let player_direction = 0;
-    let enemy_direction = 0; 
-    let px = 16, py = 16; 
-    let ex = -56, ey = 123; 
+    let px = 16, py = 16;
     const IMG_SIDE = 16; 
     let points = 0
     const animation = timestamp => {
         
         // INITIALIZE ANIMATION
         frame_count++; 
-        eframe_count++; 
         render.clearRect(0, 0, w, h); 
         render.fillRect(w / 2, h / 2, u, u); 
         render.save(); 
-        render.fillStyle = '#046'; 
+        render.scale(u, u); 
+        render.fillStyle = patterns.floor; 
         render.fillRect(0, 0, w, h); 
-        render.translate(-Math.floor(px / U_SCALE) * w, -Math.floor(py / U_SCALE) * h); 
+        render.translate(-Math.floor(px / U_SCALE) * U_SCALE, -Math.floor(py / U_SCALE) * U_SCALE); 
         
         // PLAYER PHYSICS
         let left = movement.ArrowLeft, right = movement.ArrowRight, up = movement.ArrowUp, down = movement.ArrowDown; 
@@ -91,93 +127,61 @@ window.addEventListener('DOMContentLoaded', DOMContentLoaded => {
             }
         }
 
-        for (var key in emovement) {
-            if (emovement.hasOwnProperty(key)) {     
-                if(Math.random() <= .01){
-                    emovement[key] = !emovement[key]
-                }   
-            }
-        }
-
-        let eleft = emovement.left, eright = emovement.right, eup = emovement.up, edown = emovement.down; 
-        let evx = +eright - +eleft; 
-        let evy = +edown - +eup; 
-        if(eright || eup || eleft || edown) {
-            enemy_direction = eright ? 1 : eup ? 2 : edown ? 3 : 0; 
-            if(eframe_count % 10 == 0) {
-                eframe_number = !eframe_number; 
-            }
-        }
-
         // COLLIDERS
         rigid_bodies.forEach(rigid_body => {
             if(rigid_body.y <= py + IMG_SIDE && py < rigid_body.y + rigid_body.h) {
                 if(px + IMG_SIDE <= rigid_body.x && rigid_body.x < px + IMG_SIDE + pvx) {
                     pvx = 0; 
                     px = rigid_body.x - IMG_SIDE; 
+                    console.log("hi")
                 }
                 if(rigid_body.x + rigid_body.w <= px && px + pvx < rigid_body.x + rigid_body.w) {
                     pvx = 0; 
                     px = rigid_body.x + rigid_body.w; 
+                    console.log("hello")
                 }
             }
             if(rigid_body.x <= px + IMG_SIDE && px <= rigid_body.x + rigid_body.w) {
                 if(py + IMG_SIDE <= rigid_body.y && rigid_body.y < py + IMG_SIDE + pvy) {
                     pvy = 0; 
                     py = rigid_body.y - IMG_SIDE; 
+                    console.log("sup")
                 }
                 if(rigid_body.y + rigid_body.h <= py && py + pvy < rigid_body.y + rigid_body.h) {
                     pvy = 0; 
-                    py = rigid_body.y + rigid_body.h; 
-                }
-            }
-
-            if(rigid_body.y <= ey + IMG_SIDE && ey < rigid_body.y + rigid_body.h) {
-                if(ex + IMG_SIDE <= rigid_body.x && rigid_body.x < ex + IMG_SIDE + evx) {
-                    evx = 0; 
-                    ex = rigid_body.x - IMG_SIDE; 
-                    emovement.right = false
-                }
-                if(rigid_body.x + rigid_body.w <= ex && ex + evx < rigid_body.x + rigid_body.w) {
-                    evx = 0; 
-                    ex = rigid_body.x + rigid_body.w; 
-                    emovement.left = false
-                }
-            }
-            if(rigid_body.x <= ex + IMG_SIDE && ex <= rigid_body.x + rigid_body.w) {
-                if(ey + IMG_SIDE <= rigid_body.y && rigid_body.y < ey + IMG_SIDE + evy) {
-                    evy = 0; 
-                    ey = rigid_body.y - IMG_SIDE; 
-                    emovement.down = false
-                }
-                if(rigid_body.y + rigid_body.h <= ey && ey + evy < rigid_body.y + rigid_body.h) {
-                    evy = 0; 
-                    ey = rigid_body.y + rigid_body.h; 
-                    emovement.up = false
+                    py = rigid_body.y + rigid_body.h;
+                    console.log("howdy")
                 }
             }
         }); 
         px += pvx; 
-        py += pvy; 
-        ex += evx; 
-        ey += evy;
-
-        let distance = Math.sqrt(Math.pow((px+IMG_SIDE/2)-(ex+IMG_SIDE/2),2)+Math.pow((py+IMG_SIDE/2)-(ey+IMG_SIDE/2),2))
-        if(distance <= IMG_SIDE){
-            ex = Math.floor(Math.random() * (238 - -126) ) + -126
-            ey = Math.floor(Math.random() * (238 - -126) ) + -126
-            points++
+        py += pvy;
+        if(pvx || pvy) {
+            send(JSON.stringify({x: px, y: py, direction: player_direction, frame_number: frame_number})); 
         }
         
         // RENDER DYNAMIC OBJECTS
-        render.fillStyle = '#052'; 
+        render.fillStyle = patterns.building; 
         rigid_bodies.forEach(rigid_body => {
-            render.fillRect(rigid_body.x * u, rigid_body.y * u, rigid_body.w * u, rigid_body.h * u); 
+            render.fillRect(rigid_body.x, rigid_body.y, rigid_body.w, rigid_body.h); 
         }); 
-        render.drawImage(player_avatar, +frame_number * IMG_SIDE, player_direction * IMG_SIDE, IMG_SIDE, IMG_SIDE, px * u, py * u, IMG_SIDE * u, IMG_SIDE * u); 
-        render.drawImage(enemy_avatar, +eframe_number * IMG_SIDE, enemy_direction * IMG_SIDE, IMG_SIDE, IMG_SIDE, ex * u, ey * u, IMG_SIDE * u, IMG_SIDE * u); 
 
-        render.restore();
+        Object.values(enemies).forEach(enemy => {
+            render.drawImage(enemy_avatar, +enemy.frame_number * IMG_SIDE, enemy.direction * IMG_SIDE, IMG_SIDE, IMG_SIDE, enemy.x, enemy.y, IMG_SIDE, IMG_SIDE); 
+            let distance = Math.sqrt(Math.pow((px+IMG_SIDE/2)-(enemy.x+IMG_SIDE/2),2)+Math.pow((py+IMG_SIDE/2)-(enemy.y+IMG_SIDE/2),2))
+            if(distance <= IMG_SIDE) {
+                console.log('COLLISION');
+                px = Math.floor(Math.random() * (238 - -126) ) + -126
+                py = Math.floor(Math.random() * (238 - -126) ) + -126
+                points++
+                send(JSON.stringify({x: px, y: py, direction: player_direction, frame_number: frame_number}));
+            }
+        }); 
+
+        render.drawImage(player_avatar, +frame_number * IMG_SIDE, player_direction * IMG_SIDE, IMG_SIDE, IMG_SIDE, px, py, IMG_SIDE, IMG_SIDE); 
+
+        render.restore()
+
         render.fillStyle = '#b00'
         render.font = "bold 30px Arial";
         render.fillText(`${points} TAGS`, 16, 40);
