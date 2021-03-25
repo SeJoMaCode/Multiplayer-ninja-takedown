66f41066ff83beab645e68ffb302a5db127eed37
SeJoMaCode <sethmanche02@gmail.com>
2021-03-24 19:47:20 -0500

    main gameplay mechanics

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 window.addEventListener('DOMContentLoaded', DOMContentLoaded => {
 
-    const socket = new WebSocket('wss://southwestern.media/game_dev'); 
-    socket.addEventListener('open', open => {
-        console.log('WEBSOCKET STARTED'); 
-    }); 
-
     // INITIALIZE CANVAS
     const render = document.querySelector('canvas').getContext('2d'); 
     const U_SCALE = 128; 
@@ -23,21 +18,39 @@ window.addEventListener('DOMContentLoaded', DOMContentLoaded => {
     player_avatar.src = 'images/player_spritesheet.png';
 
     // PLAYER INPUT
-    const movement = {ArrowRight: false, ArrowLeft: false, ArrowDown: false, ArrowUp: false}; 
+    const movement = {ArrowRight: false, ArrowLeft: false, ArrowDown: false, ArrowUp: false, Space: false, KeyW: false, KeyA: false, KeyS: false, KeyD: false,}; 
     document.addEventListener('keydown', keydown => {
-        if(movement.hasOwnProperty(keydown.key)) {
-            movement[keydown.key] = true; 
+        if(movement.hasOwnProperty(keydown.code)) {
+            movement[keydown.code] = true; 
         }
     }); 
     document.addEventListener('keyup', keyup => {
-        if(movement.hasOwnProperty(keyup.key)) {
-            movement[keyup.key] = false; 
+        if(movement.hasOwnProperty(keyup.code)) {
+            movement[keyup.code] = false; 
         }
     });
 
+    let frame_number = false; 
+    let frame_count = 0;
+    let player_direction = 0;
+    const IMG_SIDE = 16; 
+    let points = 0
+    let px = Math.floor(Math.random() * (238 - -126) ) + -126, py = Math.floor(Math.random() * (238 - -126) ) + -126;
     const GAME = 'ninja_top_down'; 
-    const NAME = Math.random().toString(); 
+    const NAME = Math.random().toString();
+    const PCOLORH = ('00000'+(parseFloat(NAME)*(1<<24)|0).toString(16)).slice(-6).match(/.{1,2}/g)
+    const PCOLOR = [
+        parseInt(PCOLORH[0], 16),
+        parseInt(PCOLORH[1], 16),
+        parseInt(PCOLORH[2], 16)
+    ]
     const enemies = {}; 
+    const socket = new WebSocket('wss://southwestern.media/game_dev'); 
+    socket.addEventListener('open', open => {
+        console.log('WEBSOCKET STARTED'); 
+        send(JSON.stringify({x: px, y: py, direction: player_direction, frame_number: frame_number}));
+        send('connected')
+    }); 
     const send = message => {
         socket.send(JSON.stringify({Game: GAME, Name: NAME, Message: message})); 
     }; 
@@ -51,13 +64,23 @@ window.addEventListener('DOMContentLoaded', DOMContentLoaded => {
             delete enemies[parsed.Name]; 
             return; 
         }
-        enemies[parsed.Name] = JSON.parse(parsed.Message); 
+        if(parsed.Message === 'connected') {
+            send(JSON.stringify({x: px, y: py, direction: player_direction, frame_number: frame_number}));
+            return; 
+        }
+        if(parsed.Message === NAME) {
+            console.log('tagged');
+            px = Math.floor(Math.random() * (238 - -126) ) + -126, py = Math.floor(Math.random() * (238 - -126) ) + -126;
+            send(JSON.stringify({x: px, y: py, direction: player_direction, frame_number: frame_number}));
+            return;
+        }
+        if(parsed.Message[0] === '{'){
+            enemies[parsed.Name] = JSON.parse(parsed.Message);
+        }
     }); 
-    const enemy_avatar = new Image(); 
-    enemy_avatar.src = 'images/enemy_spritesheet.png';
-    socket.addEventListener('beforeunload', beforeunload => {
+    window.addEventListener('unload', unload => {
         send('goodbye'); 
-        beforeunload['returnValue'] = null; 
+        unload['returnValue'] = null; 
     }); 
 
     const patterns = {};
@@ -96,15 +119,15 @@ window.addEventListener('DOMContentLoaded', DOMContentLoaded => {
     rigid_bodies.push(new Rigid_Body(183, 64, 32, 32))
     rigid_bodies.push(new Rigid_Body(145, 154, 32, 32))
 
+    let target
+
     // ANIMATION LOOP
-    let frame_number = false; 
-    let frame_count = 0;
-    let player_direction = 0;
-    let px = 16, py = 16;
-    const IMG_SIDE = 16; 
-    let points = 0
     const animation = timestamp => {
         
+        if(!target && Object.keys(enemies).length > 0){
+            target = Object.keys(enemies)[Math.floor(Object.keys(enemies).length * Math.random())]
+        }
+
         // INITIALIZE ANIMATION
         frame_count++; 
         render.clearRect(0, 0, w, h); 
@@ -113,10 +136,11 @@ window.addEventListener('DOMContentLoaded', DOMContentLoaded => {
         render.scale(u, u); 
         render.fillStyle = patterns.floor; 
         render.fillRect(0, 0, w, h); 
-        render.translate(-Math.floor(px / U_SCALE) * U_SCALE, -Math.floor(py / U_SCALE) * U_SCALE); 
+        const tx = -Math.floor(px / U_SCALE) * U_SCALE, ty = -Math.floor(py / U_SCALE) * U_SCALE
+        render.translate(tx, ty); 
         
         // PLAYER PHYSICS
-        let left = movement.ArrowLeft, right = movement.ArrowRight, up = movement.ArrowUp, down = movement.ArrowDown; 
+        let left = movement.ArrowLeft || movement.KeyA, right = movement.ArrowRight || movement.KeyD, up = movement.ArrowUp || movement.KeyW, down = movement.ArrowDown || movement.KeyS; 
         let pvx = +right - +left; 
         let pvy = +down - +up; 
         if(right || up || left || down) {
@@ -163,33 +187,58 @@ window.addEventListener('DOMContentLoaded', DOMContentLoaded => {
 
         render.drawImage(player_avatar, +frame_number * IMG_SIDE, player_direction * IMG_SIDE, IMG_SIDE, IMG_SIDE, px, py, IMG_SIDE, IMG_SIDE); 
 
-        var imageData = render.getImageData(px * u, py * u, IMG_SIDE * u, IMG_SIDE * u);
-        for (var i=0;i<imageData.data.length;i+=4) {
-            if(imageData.data[i]==255 && imageData.data[i+1]==0 && imageData.data[i+2]==0){
-                imageData.data[i]=0;
-                imageData.data[i+1]=0;
-                imageData.data[i+2]=255;
+        let pimageData = render.getImageData(tx * u + px * u, ty * u + py * u, IMG_SIDE * u, IMG_SIDE * u);
+        for (let i=0;i<pimageData.data.length;i+=4) {
+            if(pimageData.data[i]==255 && pimageData.data[i+1]==0 && pimageData.data[i+2]==0){
+                pimageData.data[i]=PCOLOR[0];
+                pimageData.data[i+1]=PCOLOR[1];
+                pimageData.data[i+2]=PCOLOR[2];
             }
         }
-        render.putImageData(imageData, px * u, py * u);
+        render.putImageData(pimageData, tx * u + px * u, ty * u + py * u);
 
+        let index = 0
         Object.values(enemies).forEach(enemy => {
-            render.drawImage(enemy_avatar, +enemy.frame_number * IMG_SIDE, enemy.direction * IMG_SIDE, IMG_SIDE, IMG_SIDE, enemy.x, enemy.y, IMG_SIDE, IMG_SIDE); 
-            let distance = Math.sqrt(Math.pow((px+IMG_SIDE/2)-(enemy.x+IMG_SIDE/2),2)+Math.pow((py+IMG_SIDE/2)-(enemy.y+IMG_SIDE/2),2))
-            if(distance <= IMG_SIDE) {
-                console.log('COLLISION');
-                px = Math.floor(Math.random() * (238 - -126) ) + -126
-                py = Math.floor(Math.random() * (238 - -126) ) + -126
-                points++
-                send(JSON.stringify({x: px, y: py, direction: player_direction, frame_number: frame_number}));
+            render.drawImage(player_avatar, +enemy.frame_number * IMG_SIDE, enemy.direction * IMG_SIDE, IMG_SIDE, IMG_SIDE, enemy.x, enemy.y, IMG_SIDE, IMG_SIDE); 
+            
+            const COLORH = ('00000'+(Object.keys(enemies)[index]*(1<<24)|0).toString(16)).slice(-6).match(/.{1,2}/g)
+            const COLOR = [
+                parseInt(COLORH[0], 16),
+                parseInt(COLORH[1], 16),
+                parseInt(COLORH[2], 16)
+            ]
+
+            let imageData = render.getImageData(tx * u + enemy.x * u, ty * u + enemy.y * u, IMG_SIDE * u, IMG_SIDE * u + 5);
+            for (let i=0;i<imageData.data.length;i+=4) {
+                if(imageData.data[i]==255 && imageData.data[i+1]==0 && imageData.data[i+2]==0){
+                    imageData.data[i]=COLOR[0];
+                    imageData.data[i+1]=COLOR[1];
+                    imageData.data[i+2]=COLOR[2];
+                }
             }
+            render.putImageData(imageData, tx * u + enemy.x * u, ty * u + enemy.y * u);
+            let tagged = false
+            let distance = Math.sqrt(Math.pow((px+IMG_SIDE/2)-(enemy.x+IMG_SIDE/2),2)+Math.pow((py+IMG_SIDE/2)-(enemy.y+IMG_SIDE/2),2))
+            if(distance <= IMG_SIDE && !tagged && Object.keys(enemies)[index] === target) {
+                if(movement.Space){
+                    tagged = true
+                    console.log(`tagged ${Object.keys(enemies)[index]}`)
+                    send(Object.keys(enemies)[index]);
+                    target = Object.keys(enemies)[Math.floor(Object.keys(enemies).length * Math.random())]
+                    points++
+                }
+            }
+            index++;
         }); 
 
 
         render.restore()
+
         render.fillStyle = '#b00'
         render.font = "bold 30px Arial";
         render.fillText(`${points} TAGS`, 16, 40);
+        render.fillText(`Name: ${NAME}`, 16, 70)
+        render.fillText(`Target: ${target}`, 16, 100)
         window.requestAnimationFrame(animation); 
     }; 
     window.requestAnimationFrame(animation); 
